f1e1fcc635a1972c08342d0b6676efc3b8ab9b55
uliana <ulianaevstigneeva@yandex.ru>
2024-06-01 12:48:31 +0300

    done with enum

--- SwitchedColors/ViewController.swift
+++ SwitchedColors/ViewController.swift
@@ -7,7 +7,14 @@
 
 import UIKit
 
+//MARK: Colors enum
 class ViewController: UIViewController {
+    enum ColorsType {
+        case red
+        case green
+        case blue
+    }
+    
 //MARK: Palette
     @IBOutlet var colorView: UIView!
     
@@ -21,34 +28,23 @@ class ViewController: UIViewController {
     @IBOutlet var greenIntensity: UILabel!
     @IBOutlet var blueIntensity: UILabel!
     
-
+//MARK: View did load
     override func viewDidLoad() {
         super.viewDidLoad()
         colorView.layer.cornerRadius = 12
         
-        sliderRed.minimumTrackTintColor = UIColor(
-            red: CGFloat(sliderRed.value),
-            green: 0,
-            blue: 0,
-            alpha: 1
-        )
-        sliderGreen.minimumTrackTintColor = UIColor(
-            red: 0,
-            green: CGFloat(sliderGreen.value),
-            blue: 0,
-            alpha: 1
-        )
-        sliderBlue.minimumTrackTintColor = UIColor(
-            red: 0,
-            green: 0,
-            blue: CGFloat(sliderBlue.value),
-            alpha: 1
-            )
-    
-        setText()
+        setSliderColor(for: .red)
+        setSliderColor(for: .green)
+        setSliderColor(for: .blue)
+        
+        setText(for: .red)
+        setText(for: .green)
+        setText(for: .blue)
+        
         setViewColor()
     }
-    
+
+    //MARK: Settings Methods
     private func setViewColor() {
            colorView.backgroundColor = UIColor(
             red: CGFloat(sliderRed.value),
@@ -57,43 +53,60 @@ class ViewController: UIViewController {
             alpha: 1
            )
        }
-    private func setText() {
-        redIntensity.text = String(format: "%.2f", sliderRed.value)
-        greenIntensity.text = String(format: "%.2f", sliderGreen.value)
-        blueIntensity.text = String(format: "%.2f", sliderBlue.value)
-        
+    
+    private func setText(for color: ColorsType) {
+        switch color {
+        case .red:
+            redIntensity.text = String(format: "%.2f", sliderRed.value)
+        case .green:
+            greenIntensity.text = String(format: "%.2f", sliderGreen.value)
+        case .blue:
+            blueIntensity.text = String(format: "%.2f", sliderBlue.value)
+        }
+    }
+    
+    private func setSliderColor(for color: ColorsType) {
+        switch color {
+        case .red:
+            sliderRed.minimumTrackTintColor = UIColor(
+                red: CGFloat(sliderRed.value),
+                green: 0,
+                blue: 0,
+                alpha: 1
+                )
+        case .green:
+            sliderGreen.minimumTrackTintColor = UIColor(
+                red: 0,
+                green: CGFloat(sliderGreen.value),
+                blue: 0,
+                alpha: 1
+            )
+        case .blue:
+            sliderBlue.minimumTrackTintColor = UIColor(
+                red: 0,
+                green: 0,
+                blue: CGFloat(sliderBlue.value),
+                alpha: 1
+                )
+        }
     }
     
 //MARK: Slaiders Change Methods
     @IBAction func changeRedColor() {
-        setText()
-        setViewColor ()
-        sliderRed.minimumTrackTintColor = UIColor(
-            red: CGFloat(sliderRed.value),
-            green: 0,
-            blue: 0,
-            alpha: 1
-        )
+        setText(for: .red)
+        setSliderColor(for: .red)
+        setViewColor()
     }
+    
     @IBAction func changeGreenSlider(_ sender: UISlider) {
-        setText()
-        setViewColor ()
-        sliderGreen.minimumTrackTintColor = UIColor(
-            red: 0,
-            green: CGFloat(sliderGreen.value),
-            blue: 0,
-            alpha: 1
-        )
+        setText(for: .green)
+        setSliderColor(for: .green)
+        setViewColor()
     }
     @IBAction func changeBlueSlider(_ sender: UISlider) {
-        setText()
-        setViewColor ()
-        sliderBlue.minimumTrackTintColor = UIColor(
-            red: 0,
-            green: 0,
-            blue: CGFloat(sliderBlue.value),
-            alpha: 1
-            )
+        setText(for: .blue)
+        setSliderColor(for: .blue)
+        setViewColor()
     }
 }
 
